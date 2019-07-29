f08276bc7703dd2b3644c0fb867643ea6cd1d783
swathijavvaji <swathi.javvaji@hotmail.com>
2019-07-29 14:05:47 +0200

    Added a sample line

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+public class Hello{
+
+	public static void main(String[] args){
+
+		for(int i=1;i<=10;i++){
+
+			System.out.println("Hello World..."+i);
+			System.out.println("Hello World. It's me again..."+i);
+
+		}
+		for(int i=1;i<=20;i++){
+
+			System.out.println("Hello World......"+i);
+
+		}
+	
+	}
+
+}
